5c030c0006b6453c120c657ab5ce456d4a093e60
Hillary Spitzer <hspitzer2@gmail.com>
2019-06-25 17:45:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:language]
+programmer_hash[:dennis_ritchie][:languages][0]
 end
 # return to a string: using interploation, index, or to_a_stribg?
 
